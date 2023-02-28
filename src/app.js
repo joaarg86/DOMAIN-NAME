d7e401175abdeee4c995d3292b81760482d4bf87
joaarg86 <johanna.e.escobar@icloud.com>
2023-02-28 08:24:11 +0000

    new

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//write your code here"
+
+function generateName() {
+  let subject = ["Johanna", "Jessica", "Nicole", "Elizabeth"];
+  let pronoun = ["is", "must be"];
+  let adjetive = ["beautiful", "nice", "cool", "pretty"];
+
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adjetive.length);
+
+  let result =
+    " " +
+    subject[subjIndex] +
+    " " +
+    pronoun[proIndex] +
+    " " +
+    adjetive[adjIndex];
+
+  return result;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("name").innerHTML = generateName();
 };
+
+console.log("Hello Rigo from the console!");
